7abf0ff013f33e3071832cbf041adc8758c6286a
joangui <joan@ac.upc.edu>
2016-02-24 12:52:00 +0100

    Added files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
+# WikiParser is free software developed by Joan Guisado-Gámez: 
+# you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Wikiparser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
 
 
 echo ""
